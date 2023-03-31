edcbbde926de43dab2b563cbbb5726e959fd4218
Micael-Macedo <micaelm2009@hotmail.com>
2023-03-31 10:48:10 -0300

    feat: add animate while scroll

--- script.js
+++ script.js
@@ -26,3 +26,16 @@ $('.cardOccupation').hover( function () {
 $('.cardOccupation').mouseout( function () {
     $(this).removeClass("animate__animated animate__jello");
 });
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) =>{
+        console.log(entry);
+        if (entry.isIntersecting){
+            entry.target.classList.add('show')
+        }else{
+            entry.target.classList.remove('show')
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) =>observer.observe(el));
