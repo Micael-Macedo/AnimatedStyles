fbe999948697bd0f7e5f9ede8b1477f69a97c170
Micael Macedo <micaelm2009@hotmail.com>
2023-03-30 21:41:51 -0300

    feat: add project

---
+++ script.js
@@ -0,0 +1,23 @@
+$('nav li').hover( function () {
+    $(this).addClass("animate__animated animate__tada");
+});
+$('nav li').mouseout( function () {
+    $(this).removeClass("animate__animated animate__tada");
+});
+$('.primary').hover( function () {
+    $(this).addClass("animate__animated animate__heartBeat");
+})
+$('.primary').mouseout( function () {
+    $(this).removeClass("animate__animated animate__heartBeat");
+});
+$('.listSkills div').hover( function () {
+    $(this).addClass("animate__animated animate__bounceIn");
+})
+$('.listSkills div').mouseout( function () {
+    $(this).removeClass("animate__animated animate__bounceIn");
+});
+$('.mobile button').click(function () { 
+    $('mobil')
+    $('.mobile i').toggleClass("fa-bars");
+    $('.mobile i').toggleClass("fa-x animate__animated animate__fadeIn");
+});
